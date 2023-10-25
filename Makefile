78733db32c5866288e60de3f2fb344b3f22f36e0
Ivan Morozov <xmorozov@gmail.com>
2023-10-25 14:50:36 +0300

    r

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ lint:
 	black --line-length=100 .
 	flake8 .
 	isort .
+	npx prettier . --write
 
 deps:
 	source venv/bin/activate && python3 -m pip install --default-timeout=100 -r requirements.txt && deactivate
--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from aiohttp import web
 
 import settings
 
+reptiloid = "https://storage.yandexcloud.net/reptiloid/reptiloid1.jpg"
+family = "https://storage.yandexcloud.net/reptiloid/family.jpg"
+
 with open("main.html") as main_page_file:
     main_page = main_page_file.read()
 
 
 async def handle_main(request):
-    return web.Response(text=main_page, content_type="text/html")
+    return web.Response(text=main_page.format(reptiloid), content_type="text/html")
+
+
+async def handle_family(request):
+    return web.Response(text=main_page.format(family), content_type="text/html")
 
 
 def main():
@@ -23,6 +30,7 @@ def main():
     app.add_routes(
         [
             web.get("/", handle_main),
+            web.get("/family/", handle_family),
         ]
     )
 
